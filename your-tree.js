f50d01cc3658a2610f2078252dcc11b11c0d93f3
classlfz <classlfz@qq.com>
2020-09-10 11:33:06 +0800

    :bug: fix publish

---
+++ your-tree.js
@@ -0,0 +1,73 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { LitElement, html, customElement, property, css } from 'lit-element';
+import './your-tree-mirror';
+let YourTree = class YourTree extends LitElement {
+    constructor() {
+        super(...arguments);
+        this.data = [];
+        this.show = false;
+    }
+    _toggleShow() {
+        this.show = !this.show;
+    }
+    _dispatchClickEvent(event) {
+        const target = event.target;
+        const customEvent = new CustomEvent('item-click', {
+            detail: {
+                id: target.dataset.id
+            },
+            bubbles: true,
+            composed: true
+        });
+        this.dispatchEvent(customEvent);
+    }
+    render() {
+        return html `
+      <div class="container">
+        ${this.data.map(item => {
+            return html `
+            ${item.children && item.children.length > 0 ? html `
+              <div class="item" @click="${this._toggleShow}">${this.show ? '▾ ' : '▸ '}${item.name}</div>
+              <div class="children" .hidden="${!this.show}">
+                <your-tree-mirror .data="${item.children}"></your-tree-mirror>
+              </div>
+            ` : html `
+              <div class="item" data-id="${item.id}" @click="${this._dispatchClickEvent}">▪︎ ${item.name}</div>
+            `}
+          `;
+        })}
+      </div>
+    `;
+    }
+};
+YourTree.styles = css `
+    :host {
+      display: flex;
+    }
+    .item {
+      cursor: pointer;
+    }
+    .children {
+      padding-left: 24px;
+    }
+  `;
+__decorate([
+    property({
+        type: Array
+    })
+], YourTree.prototype, "data", void 0);
+__decorate([
+    property({
+        type: Boolean
+    })
+], YourTree.prototype, "show", void 0);
+YourTree = __decorate([
+    customElement('your-tree')
+], YourTree);
+export { YourTree };
+//# sourceMappingURL=your-tree.js.map
